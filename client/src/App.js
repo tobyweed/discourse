595a658a86efd51b6331bfb0970eee70429581ed
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-01-20 20:42:30 -0500

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-	state = {
-		data: '1'
-	};
+import NavigationBar from './components/navbar/navbar';
+import Proposition from './components/proposition/proposition';
+import BackgroundInformation from './components/background/background'
+import CreateBenefit from './components/benefits/createBenefit/createBenefit'
 
-	componentDidMount() {
-		// Call our fetch function below once the component mounts
-		this.callBackendAPI()
-			.then(res => this.setState({ data: res.express }))
-			.catch(err => console.log(err));
-	}
-	// Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-	callBackendAPI = async () => {
-		const response = await fetch('/express_backend');
-		const body = await response.json();
 
-		if (response.status !== 200) {
-			throw Error(body.message);
-		}
-		return body;
-	};
-
-	render() {
-		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
-				</header>
-				<p className="App-intro">{this.state.data}</p>
-			</div>
-		);
-	}
+function App() {
+  return (
+    <div>
+      <NavigationBar /> 
+      <h1>Proposition:</h1>
+      <Proposition  prop='THIS 
+      IS A TEST 
+      
+      PROPOSITION'/>
+      <h1>Background:</h1>
+      <BackgroundInformation />
+      <CreateBenefit />
+    </div>
+    
+  );
 }
 
 export default App;
