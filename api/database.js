a5a44c1e93b343b8260fc8d6164136e711cae081
Toby Weed <tobyweed@gmail.com>
2020-01-20 21:33:27 -0500

    populated database

--- api/database.js
+++ api/database.js
@@ -21,18 +21,10 @@ let db = new sqlite3.Database(DBSOURCE, err => {
 					// Table already created
 				} else {
 					// Table just created, creating some rows
-					// var insert =
-					// 	'INSERT INTO user (name, email, password) VALUES (?,?,?)';
-					// db.run(insert, [
-					// 	'admin',
-					// 	'admin@example.com',
-					// 	md5('admin123456')
-					// ]);
-					// db.run(insert, [
-					// 	'user',
-					// 	'user@example.com',
-					// 	md5('user123456')
-					// ]);
+					var insert =
+						'INSERT INTO arg (name, description, benefit) VALUES (?,?,?)';
+					db.run(insert, ['arg1', 'lorem', true]);
+					db.run(insert, ['arg2', 'lorem', true]);
 				}
 			}
 		);
